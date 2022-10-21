b93559691f8eb8901f3bdfed021b4cf0423f5122
Humayoun Baig <humayoun.mani@gmail.com>
2022-10-21 12:00:32 +0500

    refactoring changes

--- src/api/index.js
+++ src/api/index.js
@@ -2,8 +2,6 @@ import axios from 'axios'
 
 const baseUrl = "https://api.github.com"
 
-
 export const getGistListByUserName = async (username) => {
-    const res = await axios.get(`${baseUrl}/users/${username}/gists`)
-    return res;
+    return await axios.get(`${baseUrl}/users/${username}/gists`)
 }
